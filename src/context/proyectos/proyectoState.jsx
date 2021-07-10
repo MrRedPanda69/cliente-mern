c9b3cd07dac641b9cc8555882d5cfbc29a9076ba
MrRedPanda69 <jorgeloredohdz@gmail.com>
2021-07-10 16:27:01 -0500

    dispatch for "eliminarProyecto" added

--- src/context/proyectos/proyectoState.jsx
+++ src/context/proyectos/proyectoState.jsx
@@ -8,7 +8,8 @@ import {
     OBTENER_PROYECTOS,
     AGREGAR_PROYECTO,
     VALIDAR_FORMULARIO,
-    PROYECTO_ACTUAL
+    PROYECTO_ACTUAL,
+    ELIMINAR_PROYECTO
 } from '../../types';
 
 const ProyectoState = props => {
@@ -70,6 +71,14 @@ const ProyectoState = props => {
         })
     }
 
+    // Elimina un proyecto
+    const eliminarProyecto = proyectoId => {
+        dispatch({
+            type: ELIMINAR_PROYECTO,
+            payload: proyectoId
+        })
+    }
+
     return (
         <proyectoContext.Provider
             value={{
@@ -81,7 +90,8 @@ const ProyectoState = props => {
                 getProyectos,
                 agregarProyecto, 
                 mostrarError,
-                proyectoActual
+                proyectoActual,
+                eliminarProyecto
             }}
         >
             {props.children}
